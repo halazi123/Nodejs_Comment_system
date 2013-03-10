dd5224c04af16cefda7d516109c14fbbe85b6335
g2halazi <halazi123@hotmail.com>
2013-03-10 12:12:39 -0400

    All done

--- a1server.js
+++ a1server.js
@@ -12,7 +12,39 @@ MIME_TYPES = {
 };
 
 var topics = [];
-var count = 0;
+var tcount = 0;
+var ccount = 0;
+var r = new Object;
+
+function Topic(id, topic, link){
+	this.id = id;
+	this.topic = topic;
+	this.link = link;
+	this.vote = 0;
+	this.reply = [];
+}
+
+function Reply(id, pid, replytext){
+	this.id = id;
+	this.pid = pid;
+	this.replytext = replytext;
+	this.vote = 0;
+	this.reply = [];
+}
+
+function findReply(a, id) {
+	if (a.reply.length > 0) {
+		for (var i=0;i<a.reply.length;i++) {
+			r = findReply(a.reply[i], id);
+			if ((typeof r != "undefined") && (typeof r.reply != "undefined")) {
+				return r;
+			}
+		}
+	}
+	if (a.id == id) {
+		return a;
+	}
+};
 
 function serveFile(filePath, response) {
   fs.exists(filePath, function(exists) {
@@ -39,65 +71,139 @@ function serveFile(filePath, response) {
 }
 
 http.createServer(function(request, response) {
-	console.log(request.url);
-	switch(request.url) {
-		case '/':
-			serveFile('./index.html', response);
-			break;
-		case '/style.css':
-			serveFile('./style.css', response);
-			break;
-		case '/index.js':
-			serveFile('./index.js', response);
-			break;
-		case '/addtopic':
-			if (request.method == 'POST') {
-				var buffer = '';		
-				request.on('data', function(chunk) {
-					buffer += chunk;
-				});
-				request.on('end', function() {
-					response.writeHead(200);
-					response.end();
-					topics[count] = JSON.parse(buffer);
-					topics[count].vote = 0;
-					count++;
-				});
-			}
-			else {
-				response.writeHead(405);				
-			}
-			break;
-		case '/json':
-			if (request.method == 'GET') {
+	if (request.url == '/') {
+		serveFile('./index.html', response);
+	}
+	else if (request.url == '/style.css') {
+		serveFile('./style.css', response);
+	}
+	else if (request.url == '/index.js') {
+		serveFile('./index.js', response);
+	}
+	else if (request.url.indexOf('/addtopic') == 0) {
+		if (request.method == 'POST') {
+			var buffer = '';		
+			request.on('data', function(chunk) {
+				buffer += chunk;
+			});
+			request.on('end', function() {
+				topics[tcount] = new Topic('t' + tcount, JSON.parse(buffer).topic, JSON.parse(buffer).link);
+				tcount++;
 				response.writeHead(200, {'Content-Type':'application/json'});
-				response.end(JSON.stringify(topics[count-1]));
-			}
-			else {
-				response.writeHead(405);				
-			}
-			break;
-		case '/jsonall':
-			if (request.method == 'GET') {
-				if (count > 0) {
-					var data = '';
-					response.writeHead(200, {'Content-Type':'text/plain'});
-					for (var i=0;i<count;i++){
-						data += JSON.stringify(topics[i]) + ';';
+				response.end(JSON.stringify(topics[tcount-1]));
+			});
+		}
+		else if (request.method == 'GET') {
+			var uri = require('url').parse(request.url, true);
+			topics[tcount] = new Topic('t' + tcount, uri.query.topic, uri.query.link);
+			tcount++;
+			response.writeHead(200, {'Content-Type':'application/json'});
+			response.end();
+		}
+		else {
+			response.writeHead(405);				
+		}
+	}
+	else if (request.url.indexOf('/addreply') == 0) {
+		if (request.method == 'POST') {
+			var buffer = '';
+			request.on('data', function(chunk) {
+				buffer += chunk;
+			});
+			request.on('end', function() {
+				var o = new Object;
+				buffer = JSON.parse(buffer);
+				for (var i=0;i<tcount;i++){
+					o = findReply(topics[i], buffer.pid);
+					if ((typeof o != "undefined") && (o.id == buffer.pid)) {
+						o.reply.push(buffer);
+						o.reply[o.reply.length - 1].id = 'c' + ccount;						
+						o.reply[o.reply.length - 1].vote = 0;
+						o.reply[o.reply.length - 1].reply = [];
+						ccount++;
+						response.writeHead(200, {'Content-Type':'application/json'});
+						response.end(JSON.stringify(o.reply[o.reply.length - 1]));
+						break;
 					}
-					console.log(data);
-					response.write(data);
+				}
+			});
+		}
+		else if (request.method == 'GET') {
+			var uri = require('url').parse(request.url, true);
+			var o = new Object;
+			for (var i=0;i<tcount;i++){
+				o = findReply(topics[i], uri.query.pid);
+				if ((typeof o != "undefined") && (o.id == uri.query.pid)) {
+					o.reply.push(new Reply('c' + ccount, uri.query.pid, uri.query.replytext));
+					ccount++;
+					response.writeHead(200, {'Content-Type':'application/json'});
 					response.end();
+					break;
 				}
 			}
-			else {
-				response.writeHead(405);				
+		}
+		else {
+			response.writeHead(405);				
+		}
+	}
+	else if (request.url == '/alljson') {
+		if (request.method == 'GET') {
+			if (tcount > 0) {
+				var data = '';
+				response.writeHead(200, {'Content-Type':'text/plain'});
+				for (var i=0;i<tcount;i++){
+					data += JSON.stringify(topics[i]) + ';';
+				}
+				response.end(data);
 			}
-			break;
-		default:
-			response.writeHead(404);
-			response.end('Not found.');
-	};
+		}
+		else {
+			response.writeHead(405);				
+		}
+	}
+	else if (request.url.indexOf('/allreply/') == 0) {
+		if (request.method == 'GET') {
+			var reply = [];
+			var data = '';
+			var o = new Object;
+			var id = request.url.substring(10);	
+			for (var i=0;i<tcount;i++){
+				o = findReply(topics[i], id);
+				if ((typeof o != "undefined") && (o.id == id)) {
+					reply = o.reply;
+					for (var c=0;c<reply.length;c++) {
+						data += JSON.stringify(reply[c]) + ';';
+					}
+					response.writeHead(200, {'Content-Type':'text/plain'});
+					response.end(data);
+					break;
+				}
+			}
+		}
+		else {
+			response.writeHead(405);				
+		}
+	}
+	else if (request.url.indexOf('/vote/') == 0) {
+		if (request.method == 'GET') {
+			var o = new Object;
+			var id = request.url.substring(6);
+			for (var i=0;i<tcount;i++){
+				o = findReply(topics[i], id);
+				if ((typeof o != "undefined") && (o.id == id)) {
+					o.vote++;
+					topics[i].vote++;
+					response.writeHead(200, {'Content-Type':'text/plain'});
+					response.end(o.vote + ';t' + i + ';' + topics[i].vote);
+					break;
+				}
+			}
+		}
+	}
+	else {
+		response.writeHead(404);
+		response.end('Not found.');
+	}
 }).listen(PORT);
 
 console.log('Server running at http://127.0.0.1:' + PORT + '/');
--- index.js
+++ index.js
@@ -1,43 +1,163 @@
 // JavaScript Document
-function put(data, url) {
+function add1Topic(data) {
+	var id = '';
+	var topic = '';
+	var link = '';
+	var vote = '';
+	var vote2 = '';
+	var added = false;
+
+	$.each(data, function (key, val) {
+		if (key == 'id') {
+			id = val;
+		}
+		else if (key == 'topic') {
+			topic = val;								
+		}
+		else if (key == 'link') {
+			link = val;	
+		}
+		else if (key == 'vote') {
+			vote = val;
+		}
+	});
+	
+	if ($('#topiclist > li').length > 0) {
+		$('#topiclist > li').each(function() {
+			vote2 = $(this).children('div').children('.vote').html();
+			vote2 = vote2.split(":");
+			if (Number(vote2[1]) < Number(vote)) {
+				$(this).before('<li class="topic"><div id="' + id + '"><span style="font-weight:bold" class="clickable" onclick="showReply(this)">' + topic + 
+								'</span> (<a href="' + link + '">link</a>) <span class="vote"> votes:'+ vote + '</span><a onclick="replyBox(this)" ' +
+								'class="clickable" style="float:right">reply</a></div></li>');
+				added = true;
+				return false;
+			}
+		});
+	}
+	if (!added) {
+		$('#topiclist').append('<li class="topic"><div id="' + id + '"><span style="font-weight:bold" class="clickable" onclick="showReply(this)">' + topic + 
+								'</span> (<a href="' + link + '">link</a>) <span class="vote"> votes:'+ vote + '</span><a onclick="replyBox(this)" ' +
+								'class="clickable" style="float:right">reply</a></div></li>');
+	}
+};
+
+function add1Reply(data, p) {
+	var id = '';
+	var replytext = '';
+	var vote = '';
+	var vote2 = '';
+	var added = false;
+
+	$.each(data, function (key, val) {
+		if (key == 'id') {
+			id = val;
+		}
+		else if (key == 'replytext') {
+			replytext = val;								
+		}
+		else if (key == 'vote') {
+			vote = val;
+		}
+	});
+	
+	if ($(p).find('#' + id).length == 0) {
+		if ($(p).children('.replydiv').length > 0) {
+			$(p).children('.replydiv').each(function() {
+				vote2 = $(this).children('.vote').html();
+				if (Number(vote2) < Number(vote)) {
+					$(this).before('<div class="replydiv" id="' + id + '"><a class="clickable" onclick="voteUp(this)">' + 
+					'<img src="http://www.afghanmp3.com/wp-content/plugins/commentsvote/images/vote_up.gif"/></a>' + 
+					'<span class="vote">' + vote + '</span><br><span style="font-weight:bold" ' + 
+					'class="clickable" onclick="showReply(this)">' + 
+					replytext.replace(/(\r\n|\n|\r)/gm, "<br>") + '</span><br><a onclick="replyBox(this)"' +
+					'class="clickable" >reply</a></div>');
+					added = true;
+					return false;
+				}
+			});
+		}
+		if (!added) {
+			$(p).append('<div class="replydiv" id="' + id + '"><a class="clickable" onclick="voteUp(this)">' + 
+						'<img src="http://www.afghanmp3.com/wp-content/plugins/commentsvote/images/vote_up.gif"/></a>' + 
+						'<span class="vote">' + vote + '</span><br><span style="font-weight:bold" ' + 
+						'class="clickable" onclick="showReply(this)">' + 
+						replytext.replace(/(\r\n|\n|\r)/gm, "<br>") + '</span><br><a onclick="replyBox(this)"' +
+						'class="clickable" >reply</a></div>');
+		}
+	}
+};
+
+function put(url, data, parent) {
 	$.ajax(url, {
 		type: 'POST',
 		data: JSON.stringify(data),
 		contentType: 'application/json',
-		success: function() { 
-					 $.getJSON('/json', function (data) {
-						var items = [];
-						var i = 0;
-
-						$.each(data, function (key, val) {
-							if (i == 0) {
-								items.push('<span>' + val + '</span> ');
-							}
-							else if (i == 1) {
-								items.push('(<a href="' + val + '">link</a>) ');
-							}
-							else {
-								items.push('<span> votes: '+ val + '</span>');
-							}
-							i++;
-						});
-
-					   $('<li/>', {
-							'class': 'litopic',
-							'onclick':'showReplay()',
-							html: items.join('')
-						}).appendTo('#topiclist');
-						
-					});
+		success: function(data) { 
+					if (url == '/addtopic') {
+						add1Topic(data);
+					}
+					else {
+						add1Reply(data, parent);
+					}
 				},
-		error: function() { alert("add topic failed"); }
+		error: function() { alert("submission failed"); }
 	});
 };
 
-function showReplay() {
-		alert('yes');
-		//alert($(this).html());
-		return false;
+function showReply(t) {
+	var id = $(t).parent().attr('id');
+	$.get('/allreply/' + id, function (data) {
+		if (data.length == 0) {
+			if (($(t).siblings('.replydiv').length == 0) && ($(t).siblings('form').length == 0)) {
+				$(t).parent().append('<div class="replydiv">There is currently no reply.</div>');
+			}
+		}
+		else {
+			data = data.split(";");
+			$.each(data, function(index, value) {
+				value = JSON.parse(value);
+				add1Reply(value, $(t).parent());
+			});
+		}		
+	});
+};
+
+function replyBox(t) {
+	if ($(t).siblings('form').length == 0) {
+		if ($(t).siblings("div:contains('There is currently no reply')").length > 0) {
+			$(t).siblings("div:contains('There is currently no reply')").remove();
+		}
+		$(t).after('<form onsubmit="return replySubmit(this)">' + 
+					'<input type="hidden" name="pid" value="' + $(t).parent().attr('id') + '"/>' +
+					'<textarea name="replytext" cols="60" rows="4" maxlength="400"' + 
+					'wrap="hard" placeholder="400-character limit... " required></textarea>' +
+					'<input type="submit" value="add comment"/></form>');
+	}
+};
+
+function replySubmit(t) {
+	put('/addreply', $(t).serializeObject(), $(t).parent());
+	$(t).remove();
+	return false;
+};
+
+function voteUp(t) {
+	$.get('/vote/' + $(t).parent().attr('id'), function (data) {
+		data = data.split(";");
+		$(t).siblings('.vote').html(data[0]);
+		$('#' + data[1]).children('.vote').html('votes:' + data[2]);		
+	});	
+};
+
+function getAll() {
+	$.get('/alljson', function (data) {
+		data = data.split(";");
+		$.each(data, function(index, value) {
+			value = JSON.parse(value);
+			add1Topic(value);
+		});
+	});
 };
 
 $.fn.serializeObject = function()
@@ -59,39 +179,12 @@ $.fn.serializeObject = function()
 
 
 $(document).ready(function() {	
-	$.get('/jsonall', function (data) {
-		var items = [];
-		var i = 0;
-		data = data.split(";");
-		$.each(data, function(index, value) {
-			i = 0;
-			items = [];
-			value = JSON.parse(value);
-			$.each(value, function (key, val) {
-				if (i == 0) {
-					items.push('<span>' + val + '</span> ');
-				}
-				else if (i == 1) {
-					items.push('(<a class="title" href="' + val + '">link</a>) ');
-				}
-				else {
-					items.push('<span> votes: '+ val + '</span>');
-				}
-				i++;
-			});
-			$('<li/>', {
-				'class': 'topicli',
-				'onclick':'showReplay()',
-				html: items.join('')
-			}).appendTo('#topiclist');
-		});
-	});
+	getAll();
 	
 	$('#add').submit(function() {  
 		var formvalues = $(this).serializeObject();
-		put(formvalues, '/addtopic');
+		put('/addtopic', formvalues, null);
 		return false;
-	});
-	
+	});	
 });
 
